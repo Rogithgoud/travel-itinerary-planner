39ef25f96129b151024d3e6b4284c58cca5e26bc
Rogithgoud <rogithgoud@gmail.com>
2025-12-26 00:20:36 +0530

    feat: setup React frontend with Vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import ItineraryForm from "./components/ItineraryForm";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Travel Itinerary Planner</h1>
+      <ItineraryForm />
+    </div>
+  );
+}
+
+export default App;
